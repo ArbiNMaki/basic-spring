8f210960fd2a7cf1be98ca69fc4b66a358f601ca
Arbi Dwi Wijaya <arbi.nmaki919@gmail.com>
2024-06-30 23:42:23 +0700

    Create Scope

--- src/main/java/com/basic/spring/boot/core/ScopeConfiguration.java
+++ src/main/java/com/basic/spring/boot/core/ScopeConfiguration.java
@@ -1,7 +1,10 @@
 package com.basic.spring.boot.core;
 
+import com.basic.spring.boot.core.data.Bar;
 import com.basic.spring.boot.core.data.Foo;
+import com.basic.spring.boot.core.scope.DoubletonScope;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.config.CustomScopeConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
@@ -15,5 +18,19 @@ public class ScopeConfiguration {
     public Foo foo() {
         log.info("Create new Foo");
         return new Foo();
+    }
+
+    @Bean
+    public CustomScopeConfigurer customScopeConfigurer() {
+        CustomScopeConfigurer configurer = new CustomScopeConfigurer();
+        configurer.addScope("doubleton", new DoubletonScope());
+        return configurer;
+    }
+
+    @Bean
+    @Scope("doubleton")
+    public Bar bar() {
+        log.info("Create new Bar");
+        return new Bar();
     }
 }
