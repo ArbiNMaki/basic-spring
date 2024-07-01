d836b9db854df0e4281b375d974978d85bc95bed
Arbi Dwi Wijaya <arbi.nmaki919@gmail.com>
2024-07-01 23:09:43 +0700

    Customizing Spring Application

--- src/main/java/com/basic/spring/boot/core/application/FooApplication.java
+++ src/main/java/com/basic/spring/boot/core/application/FooApplication.java
@@ -1,6 +1,7 @@
 package com.basic.spring.boot.core.application;
 
 import com.basic.spring.boot.core.data.Foo;
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -9,10 +10,20 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class FooApplication {
 
-    public static void main(String[] args) {
-        ConfigurableApplicationContext context = SpringApplication.run(FooApplication.class, args);
-        Foo foo = context.getBean(Foo.class);
+//    public static void main(String[] args) {
+//        ConfigurableApplicationContext context = SpringApplication.run(FooApplication.class, args);
+//        Foo foo = context.getBean(Foo.class);
+//
+//        System.out.println(foo);
+//    }
 
+    public static void main(String[] args) {
+        SpringApplication application = new SpringApplication(FooApplication.class);
+        application.setBannerMode(Banner.Mode.OFF);
+
+        ConfigurableApplicationContext applicationContext = application.run(args);
+
+        Foo foo = applicationContext.getBean(Foo.class);
         System.out.println(foo);
     }
 
